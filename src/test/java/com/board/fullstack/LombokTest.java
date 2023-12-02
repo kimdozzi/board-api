e3c1a5b3f06bd3b8e1f3fda348fa5d5a04142125
kimdozzi <neo5188@gmail.com>
2023-12-02 15:57:00 +0900

    test

--- src/test/java/com/board/fullstack/LombokTest.java
+++ src/test/java/com/board/fullstack/LombokTest.java
@@ -4,4 +4,10 @@ import com.board.fullstack.domain.ResultVO;
 import org.junit.jupiter.api.Test;
 
 public class LombokTest {
+    @Test
+    public void ResultTest() throws Exception {
+
+        ResultVO resultVO = new ResultVO(0, "hong");
+        System.out.println(resultVO);
+    }
 }
